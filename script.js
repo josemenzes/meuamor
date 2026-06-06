1aa187a06ed6e6008c5710c244ac86ea5ca877ad
josemenzes <menezesjose368@gmail.com>
2026-06-06 13:03:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const body = document.body
 const heart = document.getElementById('heartInicio')
 const textStart = document.getElementById('textStart')
@@ -10,14 +9,18 @@ const radial = document.getElementById('radial')
 const heartAfter = document.getElementById('heartAfter')
 
 const roleBaixo = document.getElementById('roleBaixo')
+const boxCima = document.getElementById('boxCima')
+const boxBaixo = document.getElementById('boxBaixo')
 const textRB = document.getElementById('textRB')
 const textCima = document.getElementById('textCima')
 const textBaixo = document.getElementById('textBaixo')
+const textVirado = document.getElementById('textVirado')
 
 const topValue = document.getElementById('topValue')
 const botValue = document.getElementById('botValue')
 
 const time = document.getElementById('time')
+const timeP = document.getElementById('timeP')
 
 const h2 = document.getElementsByTagName('h2')[0]
 const ceu = document.getElementById('ceu')
@@ -28,20 +31,31 @@ const nota = document.getElementById('nota')
 const spotify = document.getElementsByTagName('iframe')[0]
 const musicP = document.getElementById('wMusic')
 
+const imagens = document.getElementById('imagens')
+
+const memorias = document.getElementById('memorias')
+const memoriasP = document.getElementsByTagName('p')[6]
+
+const carta = document.getElementById('carta')
+const footer = document.getElementsByTagName('footer')[0]
+const js = document.getElementById('js')
+
 heart.onclick = function() {
     body.style.overflowY = 'scroll'
     body.style.backgroundImage = 'radial-gradient(rgb(19, 51, 88) 0, rgb(8, 3, 10) 100%)'
-    body.style.paddingBottom = '50px'
-    body.style.minHeight = '300vh'
-
+    body.style.minHeight = 'max-content'
+    body.style.gap = '80px'
     start.style.display = 'none'
     heart.style.display = 'none'
     textStart.style.display = 'none'
 
+    footer.style.display = 'unset'
     afterClick.style.display = 'flex'
     time.style.display = 'flex'
-
-    heartAfter.style.top = `20%`
+    music.style.display = 'flex'
+    memorias.style.display = 'flex'
+    carta.style.display = 'flex'
+    js.style.display = 'unset'
 
     title.innerHTML = `Para a Ingridy.`
 
@@ -59,11 +73,24 @@ heart.onclick = function() {
 btn.onclick = function() {
     music.style.width = '420px'
     music.style.height = '400px'
-    musicP.style.top = '-40%'
     musicP.style.fontSize = '5rem'
     btn.style.display = 'none'
     nota.style.display = 'none'
     spotify.style.display = 'unset'
     spotify.style.animation = 'aparecer 1.5s ease-in-out'
-    music.style.transition = 'height 0.5s ease-in-out, width 0.5s ease-in-out'
+}
+
+js.onclick = function(){
+        title.style.animation = 'none'
+        h2.style.animation = 'none'
+        roleBaixo.style.animation = 'none'
+        boxCima.style.animation = 'none'
+        boxBaixo.style.animation = 'none'
+        timeP.style.animation = 'none'
+        textVirado.style.animation = 'none'
+        music.style.animation = 'none'
+        afterClick.style.animation = 'none'
+        imagens.style.animation = 'none'
+        memoriasP.style.animation = 'none'
+        carta.style.animation = 'none'
 }
